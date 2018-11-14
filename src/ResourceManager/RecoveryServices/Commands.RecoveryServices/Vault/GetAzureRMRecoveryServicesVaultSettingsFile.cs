a54bcbafca1c8924f137e090745a932e15d0b103
Siddharth Singh <siddharth7.singh@gmail.com>
2018-11-14 17:49:20 +0530

    updated VaultCertificateExpiryInHoursForBackup to 720

--- src/ResourceManager/RecoveryServices/Commands.RecoveryServices/Vault/GetAzureRMRecoveryServicesVaultSettingsFile.cs
+++ src/ResourceManager/RecoveryServices/Commands.RecoveryServices/Vault/GetAzureRMRecoveryServicesVaultSettingsFile.cs
@@ -44,7 +44,7 @@ namespace Microsoft.Azure.Commands.RecoveryServices
         /// <summary>
         /// Expiry in hours for generated certificate.
         /// </summary>
-        private const int VaultCertificateExpiryInHoursForBackup = 48;
+        private const int c = 720;
 
         /// <summary>
         /// Vault Credential version.
