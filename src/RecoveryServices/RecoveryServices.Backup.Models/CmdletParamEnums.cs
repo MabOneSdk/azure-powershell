5118d1b64dd0d096ca4a015043a40ee16a91661d
Sambit Rath <sarath@microsoft.com>
2019-12-30 12:15:17 +0530

    Updated cmdlets with disk exclusion parameters

--- src/RecoveryServices/RecoveryServices.Backup.Models/CmdletParamEnums.cs
+++ src/RecoveryServices/RecoveryServices.Backup.Models/CmdletParamEnums.cs
@@ -58,7 +58,9 @@ namespace Microsoft.Azure.Commands.RecoveryServices.Backup.Cmdlets.Models
     public enum RestoreVMBackupItemParams
     {
         TargetResourceGroupName,
-        OsaOption
+        OsaOption,
+        RestoreDiskList,
+        RestoreOnlyOSDisk
     }
 
     public enum RestoreFSBackupItemParams
@@ -116,6 +118,7 @@ namespace Microsoft.Azure.Commands.RecoveryServices.Backup.Cmdlets.Models
         StorageAccountName,
         BackupType,
         EnableCompression,
+        DeleteState,
         InclusionDisksList,
         ExclusionDisksList,
         ResetExclusionSettings
