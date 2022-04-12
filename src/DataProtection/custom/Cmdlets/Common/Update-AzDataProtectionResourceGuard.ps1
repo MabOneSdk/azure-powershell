a73641a67d685afe14847c0c6d966ab6872db535
Himanshu Agarwal <hiaga@microsoft.com>
2022-04-12 14:57:25 +0530

    Added support for Resource Guard create/update/remove

---
+++ src/DataProtection/custom/Cmdlets/Common/Update-AzDataProtectionResourceGuard.ps1
@@ -0,0 +1,112 @@
+﻿
+
+function Update-AzDataProtectionResourceGuard
+{   
+	[OutputType('Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Models.Api20220401.IResourceGuardResource')]
+    [CmdletBinding(PositionalBinding=$false, SupportsShouldProcess)]
+    [Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Description('Updates a resource guard belonging to a resource group')]
+
+    param(
+        [Parameter(ParameterSetName="UpdateResourceGuardOperations", Mandatory=$false, HelpMessage='Subscription Id of the resource guard')]
+        [System.String]
+        ${SubscriptionId},
+
+        [Parameter(ParameterSetName="UpdateResourceGuardOperations", Mandatory, HelpMessage='Resource Group name of the resource guard')]
+        [System.String]
+        ${ResourceGroupName},
+
+        [Parameter(ParameterSetName="UpdateResourceGuardOperations", Mandatory, HelpMessage='Name of the resource guard')]
+        [System.String]
+        ${Name},
+
+        [Parameter(ParameterSetName="UpdateResourceGuardOperations", Mandatory=$false, HelpMessage='Optional ETag')]
+        [System.String]
+        ${ETag},
+
+        [Parameter(ParameterSetName="UpdateResourceGuardOperations", Mandatory=$false, HelpMessage='The identityType to be updated in resource guard, example: SystemAssigned, None')]
+        [System.String]
+        ${IdentityType},
+        
+        [Parameter(ParameterSetName="UpdateResourceGuardOperations", Mandatory=$false, HelpMessage='Resource tags')]
+        # [Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Models.Api20220401.IDppTrackedResourceTags]
+        [Hashtable]
+        ${Tag},
+
+        [Parameter(ParameterSetName="UpdateResourceGuardOperations", Mandatory=$false, HelpMessage='List of critical operations which are not protected by this resourceGuard')]
+        [System.String[]]
+        ${VaultCriticalOperationExclusionList},
+        
+        [Parameter()]
+        [Alias('AzureRMContext', 'AzureCredential')]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSObject]
+        # The credentials, account, tenant, and subscription used for communication with Azure.
+        ${DefaultProfile},
+    
+        [Parameter(DontShow)]
+        [System.Management.Automation.SwitchParameter]
+        # Wait for .NET debugger to attach
+        ${Break},
+    
+        [Parameter(DontShow)]
+        [ValidateNotNull()]
+        [Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Runtime.SendAsyncStep[]]
+        # SendAsync Pipeline Steps to be appended to the front of the pipeline
+        ${HttpPipelineAppend},
+    
+        [Parameter(DontShow)]
+        [ValidateNotNull()]
+        [Microsoft.Azure.PowerShell.Cmdlets.DataProtection.Runtime.SendAsyncStep[]]
+        # SendAsync Pipeline Steps to be prepended to the front of the pipeline
+        ${HttpPipelinePrepend},
+    
+        [Parameter(DontShow)]
+        [System.Uri]
+        # The URI for the proxy server to use
+        ${Proxy},
+            
+        [Parameter(DontShow)]
+        [ValidateNotNull()]
+        [System.Management.Automation.PSCredential]
+        # Credentials for a proxy server to use for the remote call
+        ${ProxyCredential},
+    
+        [Parameter(DontShow)]
+        [System.Management.Automation.SwitchParameter]
+        # Use the default credentials for the proxy
+        ${ProxyUseDefaultCredentials}
+    )
+
+    process
+    {
+        $ResGuard = $null
+        if($SubscriptionId -ne $null){
+            $ResGuard = Get-AzDataProtectionResourceGuard -Name $Name -ResourceGroupName $ResourceGroupName -SubscriptionId $SubscriptionId
+        }
+        else {
+            $ResGuard = Get-AzDataProtectionResourceGuard -Name $Name -ResourceGroupName $ResourceGroupName
+        }
+        
+        if($ResGuard -eq $nul){
+            $errormsg = "ResourceGuard not Found"
+    		throw $errormsg
+        }
+        
+        $null = $PSBoundParameters.Add("Location", $ResGuard.Location)
+        #$null = $PSBoundParameters.Remove("VaultCriticalOperationExclusionList")
+        #$null = $PSBoundParameters.Add("Az.DataProtection.Internal\VaultCriticalOperationExclusionList", $VaultCriticalOperationExclusionList)
+
+        # New-AzDataProtectionResourceGuard  @PSBoundParameters
+
+        Az.DataProtection.Internal\New-AzDataProtectionResourceGuard  @PSBoundParameters
+
+        #$null = $PSBoundParameters.Remove("PolicyId")
+        #$instance = Az.DataProtection\Get-AzDataProtectionBackupInstance @PSBoundParameters
+        #$instance.Property.PolicyInfo.PolicyId = $PolicyId
+        #$null = $PSBoundParameters.Remove("BackupInstanceName")
+        #$null = $PSBoundParameters.Add("Parameter", $instance)
+        #$null = $PSBoundParameters.Add("Name", $instance.Name)
+        #
+        #Az.DataProtection.Internal\New-AzDataProtectionBackupInstance  @PSBoundParameters
+    }
+}
